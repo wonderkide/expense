8b6a2d4e0e8f2eeeb6d11b4de84213242429e6ab
wonderkide <wonderkide.sos@gmail.com>
2022-06-03 16:53:57 +0700

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    port: 8079,
-    "/": {
-      target: "http://localhost:9999/",
-      ws: false
-    },
-  }
 })
 
 
